8227acf1c377b484e0b88ad1e8b1dcbad66337e1
Nigestyh <148561307+Nigestyh@users.noreply.github.com>
2026-06-25 03:50:59 +0100

    display player and computer choice using emojis

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const choiceBtns = document.querySelectorAll(".choice-btn");
+const playerResult = document.getElementById("player-result");
+const computerResult = document.getElementById("cpu-result");
 
 let playerChoiceValue = "";
 let computerChoiceValue = "";
+
+const choiceEmoji = {
+  rock: "✊🏼",
+  paper: "✋🏼",
+  scissors: "✌🏼",
+};
 
 choiceBtns.forEach((choiceBtn) => {
   choiceBtn.addEventListener("click", () => {
     playerChoiceValue = choiceBtn.value;
     computerChoiceValue = getComputedResult();
+
+    playerResult.textContent = choiceEmoji[playerChoiceValue];
+    computerResult.textContent = choiceEmoji[computerChoiceValue];
   });
 });
+
+function getComputedResult() {
+  const computerOptionChoices = ["rock", "paper", "scissors"];
+  const computerRandomChoice =
+    computerOptionChoices[
+      Math.floor(Math.random() * computerOptionChoices.length)
+    ];
+  return computerRandomChoice;
+}
